d55ddba94b47640e299ca566dca32d050bfad241
Evan <suzukaze.hazuki2020@gmail.com>
2022-04-22 14:58:03 +0900

    feat: array

--- README.md
+++ README.md
@@ -1,9 +1,61 @@
-String!
-
-
-如果没有对应的实现的话就会报错
-
-
+数据源
 ```
-"message": "Cannot return null for non-nullable field Query.ping.",
+const dataList = [
+    {
+        title: "linux",
+        url: "https://linux.org",
+        pageViews: 333
+    },
+    {
+        title: "java",
+        url: "https://linux.org",
+        pageViews: 1
+    },
+    {
+        title: "golang",
+        url: "https://linux.org",
+        pageViews: 5523
+    },
+    {
+        title: "graphQL",
+        url: "https://linux.org",
+        pageViews: 123
+    },
+    {
+        title: "dash",
+        url: "https://linux.org",
+        pageViews: 11111
+    }
+]
+```
+
+
+定义
+```
+const typeDefs = gql`
+    type Site{
+        title: String!,
+        url: String,
+        pageViews: Int,
+    },
+    type Query {
+        hello: String!,
+        book: [String],
+        sites:[Site]
+    }
+`;
+```
+
+
+实现
+```
+const resolvers = {
+    Query: {
+        hello: () => {
+            return "GraphQL!"
+        },
+        book: ()=>['n1','n2','n3'],
+        sites: ()=> dataList
+    }
+}
 ```
--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const {ApolloServer, gql} = require("apollo-server");
 
+const dataList = [
+    {
+        title: "linux",
+        url: "https://linux.org",
+        pageViews: 333
+    },
+    {
+        title: "java",
+        url: "https://linux.org",
+        pageViews: 1
+    },
+    {
+        title: "golang",
+        url: "https://linux.org",
+        pageViews: 5523
+    },
+    {
+        title: "graphQL",
+        url: "https://linux.org",
+        pageViews: 123
+    },
+    {
+        title: "dash",
+        url: "https://linux.org",
+        pageViews: 11111
+    }
+]
+
 
 const typeDefs = gql`
+    type Site{
+        title: String!,
+        url: String,
+        pageViews: Int,
+    },
     type Query {
         hello: String!,
-        ping: String!,
-        price: Int,
-        PI: Float,
-        isVIP: Boolean
+        book: [String],
+        sites:[Site]
     }
 `;
 
@@ -17,12 +48,8 @@ const resolvers = {
         hello: () => {
             return "GraphQL!"
         },
-        // ping: () => {
-        //     return "pong";
-        // },
-        price:()=> 1 ,
-        PI: ()=> 3.14,
-        isVIP: ()=> false
+        book: ()=>['n1','n2','n3'],
+        sites: ()=> dataList
     }
 }
 
